c10df69d72d148b3e39e3102977054ea3b950560
zabronax <zabronax@gmail.com>
2025-03-11 09:48:02 +0100

    containerize example-next-app

--- frontend/example-next-app/next.config.ts
+++ frontend/example-next-app/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone" // Necessary for containerization
 };
 
 export default nextConfig;
